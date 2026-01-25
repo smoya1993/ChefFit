e2a271cad3c259fcea9c86a212cd5339e9489b45
Sergio <smoya1993@gmail.com>
2026-01-25 23:05:19 +0100

    deploy: pass DATABASE_URL + jwt env to backend container

--- server/index.js
+++ server/index.js
@@ -38,5 +38,5 @@ connectDB()
     app.listen(port, () => console.log(`Server running on port ${port}`));
   })
   .catch((err) => {
-    console.error(`Error connecting to PostgreSQL ${err}`);
+    console.error("Error connecting to PostgreSQL", err);
   });
